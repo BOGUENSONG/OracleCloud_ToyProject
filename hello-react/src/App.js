ff9f48539bc2f3e4f3009b5970d11ffb38e4396e
boguen <idsonic95@gmail.com>
2022-11-01 13:40:41 +0000

    add pm2 background start

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello This is First Page of React Bbogle
         </p>
         <a
           className="App-link"
